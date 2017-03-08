f2d58fc2d7a79f380235c72bdb50c9d38785c3f2
christiannguyen <christiannnguyen7@gmail.com>
2017-03-07 23:42:17 -0800

    changed navbar

--- app.js
+++ app.js
@@ -20,7 +20,25 @@ $(document).ready(function(){
     }, 750);
 
 	});
-
+    
+   
+    });
 	
-});
 
+const nav = document.querySelector(".navbar");
+let navOffset = screen.height;
+
+
+function fixNav() {
+    console.log(navOffset);
+    if (window.scrollY >= navOffset) {
+        nav.classList.add('displayed');
+        // document.body.style.paddingTop = nav.offsetHeight + 'px';
+    } else {
+        nav.classList.remove('displayed');
+        // document.body.style.paddingTop = 0 + 'px';
+    }
+}
+
+
+window.addEventListener('scroll', fixNav);
