e6bc345719dec4ef74a6815d4696e86675621ea1
christiannguyen <christiannnguyen7@gmail.com>
2017-03-08 16:38:01 -0800

    bug fixed

--- app.js
+++ app.js
@@ -21,10 +21,7 @@ $(document).ready(function(){
 
 	});
     
-   $('#contact-form-input').on('submit', function(e) {
-  $('#contact-form-input *').fadeOut(1000);
-  $(`<p>Looking  to chatting with you!</p>`).hide().prependTo('#contact-form-input').show('slow');
-});
+   
 
     });
 	
@@ -45,5 +42,3 @@ function fixNav() {
 }
 
 window.addEventListener('scroll', fixNav);
-
-const submitted=false;
